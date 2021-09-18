0f240c4278f00c5d2327a8070fd89b48b58a166d
burak <burakcitaci@gmail.com>
2021-09-18 09:40:23 +0200

    NG update

---
+++ geovi-fullstack-angular/ClientApp/src/models/WeatherForecast.ts
@@ -0,0 +1,12 @@
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
+
+interface FeatureServerView
+{
+  title: string;
+  description: string;
+}
